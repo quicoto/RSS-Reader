2f23e0cd2a5b8acd9fa7b97334bc2bd21582bfe5
Ricard Torres <torres.rick@gmail.com>
2021-07-10 19:34:24 +0200

    Update delete-old-items.php

--- api/delete-old-items.php
+++ api/delete-old-items.php
@@ -7,7 +7,7 @@ $feeds = $mysqli->query($query_feeds);
 
 while ($feed = $feeds->fetch_assoc()) {
   // Always leave the 10 newest items
-  $query_old_items = ("DELETE FROM ".$table_items." WHERE DATEDIFF(NOW(),date) > ".$delete_old_items_days . " AND feed_id = ".$feed['id']." ORDER BY ID DESC LIMIT 100 OFFSET 10");
+  $query_old_items = "DELETE FROM ".$table_items." WHERE DATEDIFF(NOW(),date) > ".$delete_old_items_days . " AND feed_id = ".$feed['id']." ORDER BY ID DESC LIMIT 10, 100";
 
   $mysqli->query($query_old_items);
 }
