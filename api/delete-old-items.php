1081efaa54c9d52ff11d51dd90910b314a50de77
Ricard Torres <torres.rick@gmail.com>
2021-07-18 07:45:50 +0200

    Update delete-old-items.php

--- api/delete-old-items.php
+++ api/delete-old-items.php
@@ -8,7 +8,7 @@ $feeds = $mysqli->query($query_feeds);
 while ($feed = $feeds->fetch_assoc()) {
   // Always leave the 10 newest items
   // Subqueries needed for LIMIT https://ricard.dev/how-to-delete-with-limit-and-offset-in-mysql/
-  $query_old_items = "DELETE FROM ".$table_items." WHERE ID in (SELECT ID from (select ID from ".$table_items." WHERE DATEDIFF(NOW(),date) > ".$delete_old_items_days . " AND feed_id = ".$feed['id']." ORDER BY ID DESC LIMIT 10, 100) x)";
+  $query_old_items = "DELETE FROM ".$table_items." WHERE ID in (SELECT ID from (select ID from ".$table_items." WHERE DATEDIFF(NOW(),date) > ".$delete_old_items_days . " AND is_read = 1 AND feed_id = ".$feed['id']." ORDER BY ID DESC LIMIT 10, 100) x)";
 
   $mysqli->query($query_old_items);
 }
